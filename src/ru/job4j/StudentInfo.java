2f352a1f81beecd4afd7580aadaca8c031b0238b
natural-quaternion <hJhABPaKufMZ8sS2>
2021-04-08 00:21:44 +0300

    3.2. Фиксация изменений на github [#463728]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Artem Belotelov");
+        System.out.println("01.01.1980");
+    }
+}
